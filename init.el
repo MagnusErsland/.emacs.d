466cf631216620f72a4497aa611afb3e3918f954
Magnus Ersland <magnus.ersland@student.uib.no>
2017-03-11 19:29:33 +0100

    In the beginning there was darkness

---
+++ init.el
@@ -0,0 +1,8 @@
+
+(package-initialize) 
+
+(require 'org) 
+(require 'ob-tangle) 
+
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name)))) 
+(org-babel-load-file (expand-file-name "config.org" init-dir))
